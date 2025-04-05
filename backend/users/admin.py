d1399630bbef4b431f68b2579d62b6e080f0b9d2
d <sayat.bazarbay.0@gmail.com>
2025-04-06 00:39:48 +0500

    upd admin site

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from users.models import *
+
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(CustomUser)
+admin.site.register(EventParticipant)
+admin.site.register(Memory)
+admin.site.register(Category)
+admin.site.register(MemoryMention)
+admin.site.register(Interest)
+admin.site.register(EventPhoto)
+admin.site.register(UserRole)
+admin.site.register(Review)
