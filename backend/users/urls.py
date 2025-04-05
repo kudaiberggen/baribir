831fe2a3edf02e1edef307cac4cafa8c319b0a29
d <sayat.bazarbay.0@gmail.com>
2025-04-06 00:22:26 +0500

    added views for Event

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, PasswordResetView
+from .views import RegisterView, LoginView, PasswordResetView, EventFilterView, EventCreateView, EventDetailView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
+    path('events/', EventFilterView.as_view(), name='filtered-events'),
+    path('events/create/', EventCreateView.as_view(), name='event-create'),
+    path('events/<uuid:id>/', EventDetailView.as_view(), name='event-detail'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,14 @@
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
-from rest_framework import status, views, generics
+from rest_framework import status, views, generics, permissions
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
-from .serializers import RegisterSerializer, LoginSerializer, PasswordResetSerializer
+
+from .models import Category, Event, EventParticipant
+from .serializers import RegisterSerializer, LoginSerializer, PasswordResetSerializer, EventSerializer, \
+    EventParticipantSerializer
 from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -52,3 +57,49 @@ class PasswordResetView(APIView):
             response_data = serializer.send_temporary_password()
             return Response(response_data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EventFilterView(APIView):
+    def get(self, request):
+        date = request.GET.get('date')
+        category_code = request.GET.get('category')
+        interests = request.GET.getlist('interests')
+
+        filters = Q()
+
+        if date:
+            filters &= Q(date=date)
+
+        if category_code:
+            try:
+                parent_category = Category.objects.get(code=category_code)
+                categories = Category.objects.filter(Q(parent=parent_category) | Q(id=parent_category.id))
+                filters &= Q(category__in=categories)
+            except Category.DoesNotExist:
+                return Response({'error': 'Category not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        if interests:
+            filters &= Q(category__name__in=interests)
+
+        events = Event.objects.filter(filters).distinct()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+class EventParticipantsByEventView(APIView):
+    def get(self, request, event_id):
+        event = get_object_or_404(Event, id=event_id)
+        participants = EventParticipant.objects.filter(event=event)
+        serializer = EventParticipantSerializer(participants, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class EventCreateView(generics.CreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class EventDetailView(generics.RetrieveAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    lookup_field = 'id'
