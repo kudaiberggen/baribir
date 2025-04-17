f6a9bc20676b02fea37f078d2f1221b6fe4c3d13
d <sayat.bazarbay.0@gmail.com>
2025-04-17 13:50:11 +0500

    fix model Events and EventPhoto

--- backend/users/models.py
+++ backend/users/models.py
@@ -56,15 +56,18 @@ class UserPhoto(models.Model):
 class Event(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    date = models.DateTimeField()
-    city = models.CharField(max_length=100, blank=True)
-    address = models.CharField(max_length=255, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     is_free = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     contact_email = models.EmailField(blank=True, null=True)
     contact_phone = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
 
 
 class EventPhoto(models.Model):
